4df2f56da93a3d607b2df253cce9fefed0f32c0d
Pedro-HenriqueDev <ph1305803@gmail.com>
2023-01-24 15:29:02 -0300

    first commit

---
+++ src/components/Navbar.vue
@@ -0,0 +1,149 @@
+<script>
+export default {
+  data() {
+    return {
+      offset: 0,
+      navbarSm: false,
+      navSm: false,
+    };
+  },
+  created() {
+    window.addEventListener("scroll", this.handleScroll);
+    window.addEventListener("resize", this.navbarSmShow);
+    this.navbarSmShow();
+  },
+  props: {
+    hidden: Boolean,
+  },
+  methods: {
+    handleScroll() {
+      let offset = window.scrollY;
+      this.offset = offset;
+    },
+    navbarSmShow() {
+      if (window.innerWidth <= 640) {
+        this.navbarSm = true;
+        return;
+      }
+      this.navbarSm = false;
+      return;
+    },
+    showSm() {
+      this.navSm ? (this.navSm = false) : (this.navSm = true);
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="w-full">
+    <nav
+      class="
+        navbar
+        mt-6
+        -translate-y-6
+        w-full
+        flex
+        justify-center
+        items-center
+        gap-10
+        h-16
+        text-slate-100
+      "
+      :class="[
+        offset < 100 ? 'transparent' : 'shadow',
+        !navbarSm ? 'flex' : 'hidden',
+      ]"
+    >
+      <a href="#habilidades" class="cursor-pointer">Habilidades</a>
+      <a href="#projetos" class="cursor-pointer">Projeto</a>
+      <a href="#" class="cursor-pointer">Começo</a>
+      <a href="#sobremim" class="cursor-pointer">Sobre mim</a>
+      <a href="#objetivos" class="cursor-pointer">Objetivos</a>
+
+      <div class="flex cursor-pointer gap-3 float-right">
+        <a href="https://github.com/Pedro-HenriqueDev" target="_blank"><i class="block text-3xl devicon-github-original"></i></a>
+        <a href="https://www.linkedin.com/in/pedro-henrique-6ab634229/" target="_blank"><i class="block text-3xl devicon-linkedin-plain"></i></a>
+      </div>
+    </nav>
+
+      <div v-show="navbarSm" class="float-right" @click="showSm">
+        <img class="w-10" src="../assets/img/cardapio (1).png" alt="" />
+      </div>
+
+      <div v-show="navbarSm">
+        <Transition name="fade">
+        <nav class="navbar-sm flex flex-col items-center pt-18" v-show="navSm">
+          <div
+            class="w-full h-14 flex justify-end items-center mr-5">
+            <div class="w-full flex flex-col text-center gap-8 mt-80 p-10">
+              <div>
+                <a href="#habilidades" class="cursor-pointer">Habilidades</a>
+              </div>
+              <div><a href="#projetos" class="cursor-pointer">Projeto</a></div>
+              <div><a href="#" class="cursor-pointer">Começo</a></div>
+              <div>
+                <a href="#sobremim" class="cursor-pointer">Sobre mim</a>
+              </div>
+              <div>
+                <a href="#objetivos" class="cursor-pointer">Objetivos</a>
+              </div>
+
+              <div
+                class="w-full h-auto justify-center flex cursor-pointer gap-3"
+              >
+                <a href="https://github.com/Pedro-HenriqueDev" target="_blank"><i class="block text-3xl devicon-github-original"></i></a>
+                <a href="https://www.linkedin.com/in/pedro-henrique-6ab634229/" target="_blank"><i class="block text-3xl devicon-linkedin-plain"></i></a>
+              </div>
+            </div>
+          </div>
+        </nav>
+    </Transition>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.fade-enter-active {
+  transition: all 500ms ease-out;
+}
+
+.fade-leave-active {
+  transition: all 500ms ease-in;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  transform: translateX(100px);
+  opacity: 0;
+}
+.shadow {
+  transition: all 500ms;
+  background: rgba(0, 0, 0, 0.281);
+}
+.transparent {
+  transition: all 500ms;
+  background: transparent;
+}
+.navbar {
+  animation: fade forwards alternate 500ms ease-in-out;
+  position: fixed;
+  width: 100%;
+  color: #fffffe;
+}
+.navbar-sm {
+  background: rgba(0, 0, 0, 0.281);
+  position: fixed;
+  right: 0;
+  height: 100vh;
+  width: 60%;
+  color: #fffffe;
+  z-index: 1;
+}
+.float-right {
+  position: fixed;
+  right: 0;
+  padding: 15px 20px;
+  z-index: 2;
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,167 @@
+<script>
+import Navbar from "../components/Navbar.vue";
+export default {
+  data() {
+
+    return {
+      navbarShow: false
+    }
+  },
+  components: {
+    Navbar,
+  }
+};
+</script>
+
+<template>
+  <div class="pb-80 conteudo">
+    <div  class="container-principal">
+      <Navbar :hidden="navbarShow" />
+      <div
+        class="
+          md:text-7xl
+          lg:flex-row lg:justify-around
+          text-5xl
+          mx-5
+          flex flex-col
+          items-center
+          heightConteudo
+          font-bold
+          text-white
+        "
+      >
+        <div class="flex flex-col ml-10 mt-10">
+          <div class="title -translate-x-6">
+            Pedro Henrique
+            <div class="animate-bounce subtitle text-right m-3 text-2xl">Web Developer Jr</div>
+          </div>
+          <div class="flex justify-center gap-4 mt-3 w-1/2">
+              <img
+              class="icons-animated translate-y-10 w-10"
+              style="animation-delay: 100ms;"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg"
+            />
+            
+            <img
+              class="icons-animated translate-y-10 w-10"
+              style="animation-delay: 300ms;"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg"
+            />
+            <img
+              class="icons-animated translate-y-10 w-10"
+              style="animation-delay: 500ms;"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/vuejs/vuejs-original.svg"
+            />
+            <img
+              class="icons-animated translate-y-10 w-10"
+              style="animation-delay: 700ms;"
+              src="https://cdn.jsdelivr.net/gh/devicons/devicon/icons/git/git-original.svg"
+            />
+          </div>
+        </div>
+        <div class="flex text-2xl">
+          <div class=""><img class="md:w-96 w-80" src="../assets/img/bighead.svg"></div>
+        </div>
+      </div>
+    </div>
+    <div class="main">
+      <div class="m-10">
+        <div id="sobremim" class="ancora"></div>
+        <h2 class="text-3xl text-color font-semibold">Sobre mim</h2>
+        <div class="mx-10 mt-5">
+          Em 2021 consegui realizar alguns projetos com o básico conhecimento
+          que eu tinha com HTML e CSS . Também realizei alguns cursos básicos na
+          plataforma digital ,Digital Inovation One ,a qual estou me
+          aprofundando em Desenvolvimento Web ,Back-end com a tecnologia Node.js
+        </div>
+      </div>
+      <div class="m-10">
+        <div id="objetivos" class="ancora"></div>
+        <h2 class="text-3xl text-color font-semibold">Objetivos</h2>
+        <div class="mx-10 mt-5">
+          Em 2021 consegui realizar alguns projetos com o básico conhecimento
+          que eu tinha com HTML e CSS . Também realizei alguns cursos básicos na
+          plataforma digital ,Digital Inovation One ,a qual estou me
+          aprofundando em Desenvolvimento Web ,Back-end com a tecnologia Node.js
+        </div>
+      </div>
+      <div class="m-10">
+        <div id="habilidades" class="ancora"></div>
+        <h2 class="text-3xl text-color font-semibold">Habilidades</h2>
+        <div class="mx-10 mt-5">
+          Em 2021 consegui realizar alguns projetos com o básico conhecimento
+          que eu tinha com HTML e CSS . Também realizei alguns cursos básicos na
+          plataforma digital ,Digital Inovation One ,a qual estou me
+          aprofundando em Desenvolvimento Web ,Back-end com a tecnologia Node.js
+        </div>
+      </div>
+      <div class="m-10">
+        <div id="projetos" class="ancora"></div>
+        <h2 class="text-3xl text-color font-semibold" id="projetos">
+          Projetos
+        </h2>
+        <div class="mx-10 mt-5">
+          Em 2021 consegui realizar alguns projetos com o básico conhecimento
+          que eu tinha com HTML e CSS . Também realizei alguns cursos básicos na
+          plataforma digital ,Digital Inovation One ,a qual estou me
+          aprofundando em Desenvolvimento Web ,Back-end com a tecnologia Node.js
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.icons-animated {
+  opacity: 0;
+  animation: transform forwards alternate 500ms ease-in-out;
+}
+.text-color {
+  color: #121629;
+}
+.conteudo {
+  background-color: #b8c1ec;
+}
+.title {
+  animation: fade forwards alternate 500ms ease-in-out;
+  color: #fffffe;
+}
+.subtitle {
+  color: #b8c1ec;
+}
+.container-principal {
+  width: 100%;
+  height: 110vh;
+  background: #232946;
+
+  border-bottom-right-radius: 10px;
+  border-bottom-left-radius: 10px;
+}
+.ancora {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  left: 0;
+  margin-top: -100px;
+  background-color: transparent;
+  z-index: -1;
+}
+.heightConteudo {
+  height: 85%;
+}
+@keyframes fade {
+  0% {
+    opacity: 0;
+  }
+  100% {
+    opacity: 1;
+    transform: translateX(0)
+  }
+}
+@keyframes transform {
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
